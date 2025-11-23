011a2540acdf74f43c7bd377a4a515b5efb10de4
keerthi-manur <112362224+keerthi-manur@users.noreply.github.com>
2025-11-23 17:29:44 -0500

    Refactor code by removing debug statements
    Removed debug print statements and adjusted sleep duration.

--- code.py
+++ code.py
@@ -3,48 +3,32 @@ import board
 import displayio
 import digitalio
 import microcontroller
+import supervisor
+import sys
 from fourwire import FourWire
 from adafruit_st7789 import ST7789
-import supervisor
 
-# Change this to False to hide debug print statements
-Debug = True
-
-if Debug:
-    print("Create pin called 'backlight' for LCD backlight on PA06")
 backlight = digitalio.DigitalInOut(microcontroller.pin.PA06)
 backlight.direction = digitalio.Direction.OUTPUT
-
-# Release any resources that may have been previously in use for the displays
-if Debug:
-    print("Release displays")
 displayio.release_displays()
 
-if Debug:
-    print("Create SPI Object for display")
 spi = board.LCD_SPI()
 tft_cs = board.LCD_CS
 tft_dc = board.D4
-
-if Debug:
-    print("Turn TFT Backlight On")
-# Backlight control is Active LOW
 backlight.value = False
 
 DISPLAY_WIDTH = 240
 DISPLAY_HEIGHT = 135
 
-if Debug:
-    print("Create DisplayBus")
 display_bus = FourWire(spi, command=tft_dc, chip_select=tft_cs)
 display = ST7789(
     display_bus, rotation=90, width=DISPLAY_WIDTH, height=DISPLAY_HEIGHT, rowstart=40, colstart=53
 )
 
-print("type something here:")
+print("PyKit Display Ready: ")
 
 while True:
     if supervisor.runtime.serial_bytes_available:
         received_data = input().strip()
-        print(f"Received: {received_data}")
-    time.sleep(0.1)
+        print(received_data)
+    time.sleep(0.01)
